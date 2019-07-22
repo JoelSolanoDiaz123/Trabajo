ae021fecb9494b13ed5309f3f8b8f54cf7dec4ea
JoelSolanoDiaz123 <53194333+JoelSolanoDiaz123@users.noreply.github.com>
2019-07-22 15:57:05 -0500

    Add files via upload

---
+++ App.py
@@ -0,0 +1,16 @@
+print("Ingrese un numero")
+numero = int(input("Ingrese: "))
+numero2 = int(input("ingrese: "))
+
+if numero < numero2:
+ NS = int(input("escriba de nuevo el segundo numero: "))
+if numero > nunero2:
+ resultado = numero-numero2
+ print("la resta es: ",resultado)
+else:
+    print("Su primer numero es: ",numero)
+    print("Su segundo numero es: ",numero2)
+    print("Su segundo numero es: ",NS)
+Resultado = numero+numero2
+print("la suma es de: ",Resultado)
+
